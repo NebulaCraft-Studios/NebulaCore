1fcdede1d05c76b2e44173209f0b8d20a773749d
Skullian <133133667+Skullians@users.noreply.github.com>
2024-03-27 15:25:53 +0000

    slight alterations and fixes
    ignore pom.xml xd

--- src/main/java/net/nebulacraft/nebulaships/commands/cmds/InfoCommand.java
+++ src/main/java/net/nebulacraft/nebulaships/commands/cmds/InfoCommand.java
@@ -34,7 +34,7 @@ public class InfoCommand extends SubCommand {
                 "&7------------- &r" + NebulaShips.configManager.getFile(ConfigTypes.MESSAGES).getConfig().getString("Messages.General.SERVER_NAME") + "&r&7 -------------\n"+
                         "&3Version: &r{#4821FF}" + NebulaShips.getInstance().getDescription().getVersion() + "{/#0064FF}&r\n" +
                         "&3Authors: &r{#4821FF}" + NebulaShips.getInstance().getDescription().getAuthors() + "{/#0064FF}&r\n" +
-                        "&Website: &r{#4821FF}" + NebulaShips.getInstance().getDescription().getWebsite() + "{/#0064FF}&r\n" +
+                        "&3Website: &r{#4821FF}" + NebulaShips.getInstance().getDescription().getWebsite() + "{/#0064FF}&r\n" +
                         "&3Get Started: &r{#4821FF}/nebulacraft help{/#0064FF}&r\n" +
                         "&7------------- &r" + NebulaShips.configManager.getFile(ConfigTypes.MESSAGES).getConfig().getString("Messages.General.SERVER_NAME") + "&r&7 -------------"));
     }
